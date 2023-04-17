a107f921f7d4515f6c16481da809175706f4fb9b
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2023-04-17 13:47:29 +0200

    added navbar to home

--- sidmappen/src/components/Home/Home.jsx
+++ sidmappen/src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Home.css";
+import NavigationMenu from "../NavigationMenu";
 
 const Home = () => {
   return (
     <div>
       <header>
         <h2>Fluffy Tails</h2>
-        navbar
+        <NavigationMenu />
       </header>
       <main>
         <h3>Here is some lovely Cat's that are in need of a home</h3>
