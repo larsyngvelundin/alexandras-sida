7654c52c603d2e979597528bb829a237ec68d538
d0ndada <pintoavila09@gmail.com>
2023-04-17 12:25:19 +0200

    fist commit

---
+++ sidmappen/src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <h2>Fluffy Tails</h2>
+        navbar
+      </header>
+      <main>
+        <h3>Here is some lovely Cat's that are in need of a home</h3>
+      </main>
+      <footer>&copy; 2023 Fluffy tails</footer>
+    </div>
+  );
+};
+
+export default Home;
